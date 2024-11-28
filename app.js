1cb294362185661391f4d5a7c57d494b9cc57257
Blossom Nuviadenu <nuviadenub@gmail.com>
2024-11-27 16:22:07 -0800

    my changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My first website'));
+app.get('/', (req, res) => res.send('My first website1'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
